9a2d3490eae8cbc3e665ba8964d5f615ae24c665
antonvh <antonhaninvl@gmail.com>
2022-11-30 12:18:29 +0300

    титл навигации

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navigation: FC = () => {
   return (
     <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
-      <h3 className="font-bold">Github search</h3>
+      <h3 className="font-bold">Book search</h3>
       <span className="flex justify-between w-[120px] px-0">
         <Link to="/">Home</Link>
         <Link to="/favorites">Favorites</Link>
